2af4e93268f4cfbbbd6a4f87866b0abe6ea1c9b5
Samuel ODaniels <samodan195@gmail.com>
2022-06-10 15:18:34 +0100

    Pass teests for 4 methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,33 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    self.my_each do |item|
+      return false unless yield(item)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |item|
+      return true if yield(item)
+    end
+    false
+  end
+  def my_count(member = nil)
+    count = 0
+    if block_given?
+      self.my_each do |item|
+        count += 1 if yield(item)
+      end
+    else
+      return self.length unless member
+
+      self.my_each do |item|
+        count += 1 if member == item
+      end
+    end
+    count
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +36,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return self.to_enum unless block_given?
+
+    for i in self
+      yield i
+    end
+    self
+  end
 end
